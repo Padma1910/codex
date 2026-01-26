433d08d06f4410322735f6c10f4d06d516374f7d
Padma1910 <24149036@drngpit.ac.in>
2026-01-26 18:01:25 +0530

    Create 2630-AlternatingDigitSum

---
+++ 2630-AlternatingDigitSum/2630-AlternatingDigitSum.java
@@ -0,0 +1,13 @@
+// Last updated: 1/26/2026, 6:01:24 PM
+class Solution {
+    public int alternateDigitSum(int n) {
+        String s=String.valueOf(n);
+        int sum=0;
+        for(int i=0;i<s.length();i++){
+            int d= s.charAt(i)-'0';
+            if(i%2==0) sum+=d;
+            else sum -=d;
+        }
+        return sum; 
+    }
+}
